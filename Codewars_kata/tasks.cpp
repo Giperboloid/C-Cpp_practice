66e70defe1a3e6dc497d1ac33a59659a24b02916
Aidar <56221624+Giperboloid@users.noreply.github.com>
2023-08-24 23:55:03 +0300

    Added `Descending order` Kata task

--- Codewars_kata/tasks.cpp
+++ Codewars_kata/tasks.cpp
@@ -21,3 +21,32 @@ bool is_isogram(std::string str) {
     return true;
 
 }
+
+
+/// Kata: Descending Order
+
+// Your task is to make a function that can take any non-negative integer as an argument 
+// and return it with its digits in descending order. Essentially, rearrange the digits 
+// to create the highest possible number.
+
+/* Examples:
+Input: 42145 Output: 54421
+Input: 145263 Output: 654321
+Input: 123456789 Output: 987654321 */
+
+uint64_t descendingOrder(uint64_t a)
+{
+  
+  auto char_digits{std::to_string(a)};
+  
+  std::sort(char_digits.begin(), char_digits.end(), std::greater<char>());
+  
+  uint64_t result {};
+  std::from_chars(char_digits.data(), char_digits.data() + char_digits.size(), result);
+  
+  return result;
+}
+
+
+
+    
